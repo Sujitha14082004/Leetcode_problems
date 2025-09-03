efeae72f35c1744d82ca6c4755d8aea86f1392d7
Sujitha14082004 <sujithachinnasamy414@gmail.com>
2025-09-03 10:09:27 +0530

    Time: 0 ms (100%), Space: 40.1 MB (97.57%) - LeetHub

--- 0461-hamming-distance/0461-hamming-distance.java
+++ 0461-hamming-distance/0461-hamming-distance.java
@@ -1,19 +1,5 @@
 class Solution {
     public int hammingDistance(int x, int y) {
-        int count=0;
-        String str=Integer.toBinaryString(x);
-        String str1=Integer.toBinaryString(y);
-        while(str.length()<str1.length()){
-            str="0"+str;
-        }
-        while(str1.length()<str.length()){
-            str1="0"+str1;
-        }
-        for(int i=0;i<str.length();i++){
-            if(str.charAt(i)!=str1.charAt(i)){
-                count++;
-            }
-        }
-        return count;
+        return Integer.bitCount(x^y);
     }
 }
