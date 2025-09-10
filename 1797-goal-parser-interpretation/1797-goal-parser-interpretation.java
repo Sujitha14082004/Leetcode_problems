2b537ece20f17b8543f8b9f34c78d54485685ea2
Sujitha14082004 <sujithachinnasamy414@gmail.com>
2025-09-10 18:25:17 +0530

    Time: 1 ms (63.59%), Space: 41.7 MB (62.79%) - LeetHub

---
+++ 1797-goal-parser-interpretation/1797-goal-parser-interpretation.java
@@ -0,0 +1,17 @@
+class Solution {
+    public String interpret(String command) {
+        StringBuilder ans=new StringBuilder();
+        for(int i=0;i<command.length();i++){
+            if(command.charAt(i)=='G'){
+                ans.append("G");
+            }
+            if(command.charAt(i)=='('&& command.charAt(i+1)==')'){
+                ans.append("o");
+            }
+            if(command.charAt(i)=='('&& command.charAt(i+1)=='a'){
+                ans.append("al");
+            }
+        }
+        return ans.toString();
+    }
+}
