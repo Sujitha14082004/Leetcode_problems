d729af2c20cb008266f3458f5b872a7534147ea6
Sujitha14082004 <sujithachinnasamy414@gmail.com>
2025-12-27 20:01:00 +0530

    Time: 1 ms (98.06%), Space: 44.5 MB (13.89%) - LeetHub

---
+++ 3184-count-pairs-that-form-a-complete-day-i/3184-count-pairs-that-form-a-complete-day-i.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int countCompleteDayPairs(int[] hours) {
+        int count=0;
+        for(int i=0;i<hours.length;i++){
+            for(int j=i;j<hours.length;j++){
+                if((i<j) && (hours[i]+hours[j])%24==0){
+                    count++;
+                }
+            }
+        }
+        return count;  
+    }
+}
