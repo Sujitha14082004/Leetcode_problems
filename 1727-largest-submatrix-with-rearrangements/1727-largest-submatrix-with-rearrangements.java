59256eed53b8c73d42ba5f00ddeea554a2c837d5
Sujitha14082004 <sujithachinnasamy414@gmail.com>
2026-03-17 09:45:50 +0530

    Time: 13 ms (86.96%), Space: 113.8 MB (75.65%) - LeetHub

--- 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
+++ 1727-largest-submatrix-with-rearrangements/1727-largest-submatrix-with-rearrangements.java
@@ -3,15 +3,17 @@ class Solution {
         for(int i=1;i<matrix.length;i++){
             for(int j=0;j<matrix[0].length;j++){
                 if(matrix[i][j]==1){
-                    matrix[i][j]=matrix[i-1][j]+1;
+                    matrix[i][j]+=matrix[i-1][j];
                 }
             }
         }
         int max=0;
         for(int i=0;i<matrix.length;i++){
             Arrays.sort(matrix[i]);
-            for(int j=1;j<=matrix[0].length;j++){
-                max=Math.max(max, j*matrix[i][matrix[0].length-j]);
+            for(int j=0;j<matrix[0].length;j++){
+                int height=matrix[i][j];
+                int width=matrix[0].length-j;
+                max=Math.max(max, height*width);
             }
         }
         return max;
