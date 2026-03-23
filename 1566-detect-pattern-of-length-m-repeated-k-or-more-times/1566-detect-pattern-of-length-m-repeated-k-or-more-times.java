f3a2f2815ef8426d2ed5e0b6770eaf0a17ec0d0a
Sujitha14082004 <sujithachinnasamy414@gmail.com>
2026-03-23 09:38:37 +0530

    Time: 0 ms (100%), Space: 43.1 MB (74.39%) - LeetHub

---
+++ 1566-detect-pattern-of-length-m-repeated-k-or-more-times/1566-detect-pattern-of-length-m-repeated-k-or-more-times.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean containsPattern(int[] arr, int m, int k) {
+        int count=0;
+        for(int i=0;i+m<arr.length;i++){
+            if(arr[i]==arr[i+m]){
+                count++;
+                if(count==m*(k-1)){
+                    return true;
+                }
+            }
+            else{
+                count=0;
+            }
+        }
+        return false;
+    }
+}
