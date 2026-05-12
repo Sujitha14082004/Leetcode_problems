7c04bb00383d04ae478d49cbc249e3faa8423907
Sujitha14082004 <sujithachinnasamy414@gmail.com>
2026-05-12 22:56:41 +0530

    Time: 4 ms (82.14%), Space: 46.7 MB (8.78%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        char[] x = s.toCharArray();
+        char[] y = t.toCharArray();
+        Arrays.sort(x);
+        Arrays.sort(y);
+        return Arrays.equals(x,y);
+    }
+}
