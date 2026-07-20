3b787e7e820479f7eb210f9a8f095bf79cdaca5f
Sujitha14082004 <sujithachinnasamy414@gmail.com>
2026-07-20 17:16:58 +0530

    Time: 58 ms (33.89%), Space: 47.5 MB (8.01%) - LeetHub

--- 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
+++ 0395-longest-substring-with-at-least-k-repeating-characters/0395-longest-substring-with-at-least-k-repeating-characters.java
@@ -1,17 +1,20 @@
 class Solution {
     public int longestSubstring(String s, int k) {
-       int freq[] = new int[26];
-       for(int i=0;i<s.length();i++){
-        freq[s.charAt(i)-'a']++;
-       } 
-       for(int i=0;i<s.length();i++){
-        if(freq[s.charAt(i)-'a']<k){
-            int left = longestSubstring(s.substring(0,i),k);
-            int right = longestSubstring(s.substring(i+1,s.length()),k);
-            int max = Math.max(left,right);
-            return max;
+        //base cond...
+        // if(s.length()<k) return 0;
+        // if(k<=1) return s.length();
+
+        int freq[] = new int [26];
+        for(int i=0;i<s.length();i++){
+            freq[s.charAt(i)-'a']++;
         }
-       }
-       return s.length();
+        for(int i=0;i<s.length();i++){
+            if(freq[s.charAt(i)-'a']<k){
+                int left = longestSubstring(s.substring(0, i), k);
+                int right = longestSubstring(s.substring(i+1, s.length()), k);
+                return Math.max(left, right);
+            }
+        }
+        return s.length();
     }
 }
